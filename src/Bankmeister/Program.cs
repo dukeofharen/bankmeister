b379aa2f9eca4a61e806de9b10b6566a6e32c4b4
Duco Winterwerp <duco@winterwerp.nl>
2017-07-16 17:49:02 +0200

    Initial commit

---
+++ src/Bankmeister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankmeister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
